9977370d9fc67728839515f516b9f63077affebe
Samad Rizvi <33986539+themajestickoala@users.noreply.github.com>
2019-02-12 20:44:01 -0600

    Add files via upload

--- flask_tutorial/main.py
+++ flask_tutorial/main.py
@@ -1,15 +1,11 @@
 from flask import Flask, render_template             
 app = Flask(__name__)
-@app.route("/")
+@app.route('/home', methods=['GET','POST'])
 def home():
     return render_template("home.html")
-@app.route("/salvador")
-def salvador():
-    return "Hello, Salvador"
-if __name__ == "__main__":
-    app.run(debug=True)
-@app.route("/")
+
+@app.route('/upload.php', methods=['GET','POST'])
 def upload():
     return render_template("imgReceived.html")
-if __name__ == "__main__":
+    if __name__ == "__main":
         app.run(debug=True)
